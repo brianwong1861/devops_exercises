ecdde1c6c13a3e5de9ff22e4973c5621210c20ad
brianwong1861 <brianwong1861@gmail.com>
2018-08-17 14:58:32 +0800

    Added more

--- Q3B/main.go
+++ Q3B/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+
 	"os"
 	"fmt"
 	"encoding/json"
@@ -12,6 +13,8 @@ import (
 	rs "../Q3B/utils" 
 	"io/ioutil"
 	_ "reflect"
+	redisConn "../Q3B/model/redis"
+	_ "github.com/garyburd/redigo/redis"
 
 )
 
@@ -24,34 +27,40 @@ type Record struct {
 }
 
 
-var urllist []Record
+// var urllist []Record
 var record Record
 
 
 func (p *Record) GetAllOriginalURL(w http.ResponseWriter , r *http.Request){
-			
+	
+		keysList, err := redisConn.GetKeys("*")
+		if err != nil {
+			fmt.Println("Error retrieve keys",err)
+		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(urllist)
+		json.NewEncoder(w).Encode(keysList)
+	
 }
 
 // GetOriginalURL fetches the original URL for the given encoded(short) string
 func (p *Record ) GetOriginalURL(w http.ResponseWriter, r *http.Request) {
 	
-	var results []Record	
+	// var results []Record	
+	// var res []byte
 	vars := mux.Vars(r)
 	req_url := urlPrefix + vars["urlhash"] 
-	if len(urllist) == 0 {
+	exist, _ := redisConn.Exists(req_url)
+	if exist == false {
 		w.Write([]byte("Not found this mapping domain"))
-		return 
+	} else {
+			res, err := redisConn.Get(req_url)
+			if err != nil {
+				fmt.Println(err)
+			}
+			site := "http://" + string(res)
+			http.Redirect(w, r, site, 301)
 	}
-	for _, v := range urllist {
-			if req_url == v.ShortenURL {
-				results = append(results, v)
-			} 
-		}
-	site := "http://" + results[0].URL
-	http.Redirect(w, r, site, 301)
 	
 }
 // GenerateShortURL adds URL to Record dataset and gives back shortened string
@@ -60,8 +69,12 @@ func (p *Record) GenerateShortURL(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	record.ShortenURL = urlPrefix + rs.RandStringBytes(8)
 	json.Unmarshal(body, &record)
-	urllist = append(urllist, record)
-
+	a1 := []byte(record.URL)
+	// val := []byte(record.ShortenURL) // convert string to byte slice
+	err := redisConn.SetEX(record.ShortenURL, a1)
+	if err != nil {
+		log.Fatal(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(record)
@@ -69,6 +82,7 @@ func (p *Record) GenerateShortURL(w http.ResponseWriter, r *http.Request) {
 }
 func init(){
 	_ = godotenv.Load(".env")
+
 }
 
 func main() {
